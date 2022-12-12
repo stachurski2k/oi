abd53cccdcbb6f85bbf58c7eeb0e2ed0eba23f61
stachurski2k <43575052+stachurski2k@users.noreply.github.com>
2022-12-12 17:32:35 +0100

    Add files via upload

--- kol.cpp
+++ kol.cpp
@@ -1,68 +1,125 @@
+///Autor: Stanislaw Deja
+// Zlozonosc : n^(1/2)*n
+
+
 #include<iostream>
 #include<bits/stdc++.h>
+// #include<fstream>
 using namespace std;
-int world[2000][2000];
-bool snake_world[2000][2000];
+constexpr int maxM=2007;
+constexpr int maxSegSize=1000;
+struct segment{
+    int id;
+    int first;
+    int firstsnake;
+    int size=0;
+};
+int world[maxM][maxM];
+bool snake_world[maxM][maxM];
+vector<pair<int,int>> snakecoords;
+vector<segment> seg;
+vector<int> snake;
 int s,num,m;
-vector<pair<int,int>> snake;
-vector<pair<int,int>> snake_cord;
+// ofstream fs("res");
+pair<int,int> head;
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin>>s>>num>>m;
     int a,b,c;
-    for (int i = 0; i < 2000; i++)
+    for (int i = 0; i < maxM; i++)
     {
-        for (int j = 0; j < 2000; j++)
+        for (int j = 0; j < maxM; j++)
         {
             world[i][j]=-1;
             snake_world[i][j]=false;
         }
     }
-    world[1][1]=0;
-    snake.push_back({0,0});
-    snake_cord.push_back({1,1});
-    snake_world[1][1]=true;
     for (int i = 0; i < num; i++)
     {
         cin>>a>>b>>c;
         world[a][b]=c;
     }
-    char x;
+    snake_world[1][1]=true;
+    segment s;
+    s.id=seg.size();
+    s.first=0;
+    s.size=1;
+    s.firstsnake=0;
+    snake.push_back(0);
+    head={1,1};
+    world[1][1]=s.id;
+    seg.push_back(s);
+    snakecoords.push_back({1,1});
+    char z;
     for (int i = 0; i < m; i++)
     {
-        cin>>x;
-        if(x=='Z'){
+        cin>>z;
+        if(z=='Z'){
             cin>>a>>b;
-            if(!snake_world[a][b]){
-                cout<<-1<<'\n';
+            if(snake_world[a][b]){
+                int index=world[a][b];
+                segment s=seg[index];
+                // fs<<a<<" "<<b<< " "<<s.first<<"\n";
+                pair<int,int> coord;;
+                int res=0;
+                for (int j = 0; j <s.size; j++)
+                {
+                    coord=snakecoords[j+s.first];
+                    if(coord.first==a&&coord.second==b){
+                        res=j;
+                        break;;
+                    }
+                }
+                // fs<<snake[s.firstsnake+res]<<'\n';;
+                cout<<snake[s.firstsnake+res]<<'\n';
+
             }else{
-                cout<<world[a][b]<<'\n';
+                // fs<<-11<<'\n';
+                cout<<-1<<'\n';
             }
         }else{
-            pair<int,int> head=snake_cord[snake_cord.size()-1];
-            if(x=='G')head.first--;
-            else if(x=='D')head.first++;
-            else if(x=='P')head.second++;
-            else head.second--;
-            if(world[head.first][head.second]!=-1){
-                snake.push_back({world[head.first][head.second],snake_cord.size()});
-                snake_world[head.first][head.second]=true;
-                snake_cord.push_back(head);
-            }else{
-                snake_cord.push_back(head);
-                world[snake_cord[snake[0].second].first][snake_cord[snake[0].second].second]=-1;
-                snake_world[snake_cord[snake[0].second].first][snake_cord[snake[0].second].second]=false;
-                for (int i = 0; i < snake.size(); i++)
-                {
-                    snake[i].second++;
-                    world[snake_cord[snake[i].second].first][snake_cord[snake[i].second].second]=snake[i].first;
-                    snake_world[snake_cord[snake[i].second].first][snake_cord[snake[i].second].second]=true;
+            pair<int,int> move=head;
+            if(z=='G')move.first--;
+            else if(z=='D')move.first++;
+            else if(z=='P')move.second++;
+            else move.second--;
+            head=move;
+            if(!snake_world[move.first][move.second]&&world[move.first][move.second]!=-1){
+                //found new snack to eat esssa;
+                segment lastseg=seg[seg.size()-1];
+                if(lastseg.size>=maxSegSize){
+                    segment nseg;
+                    nseg.id=seg.size();
+                    nseg.size=1;
+                    nseg.first=snakecoords.size();
+                    nseg.firstsnake=snake.size();
+                    snake.push_back(world[move.first][move.second]);
+                    world[move.first][move.second]=lastseg.id;
+                    seg.push_back(nseg);
+                }else{
+                    lastseg.size=lastseg.size+1;
+                    snake.push_back(world[move.first][move.second]);
+                    world[move.first][move.second]=lastseg.id;
+                    seg[seg.size()-1]=lastseg;
                 }
-
+                snake_world[move.first][move.second]=true;
+                snakecoords.push_back(move);
+            }else{
+                snakecoords.push_back(move);
+                pair<int,int> coord=snakecoords[seg[0].first];
+                snake_world[coord.first][coord.second]=false;
+                snake_world[move.first][move.second]=true;
+                world[coord.first][coord.second]=-1;
+                for (int j = 0; j < seg.size(); j++)
+                {
+                    seg[j].first++;
+                    coord=snakecoords[seg[j].first+seg[j].size-1];
+                    world[coord.first][coord.second]=seg[j].id;
+                }
             }
-
         }
     }
-    
-
     return 0;
 }
